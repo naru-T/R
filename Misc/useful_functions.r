48f18b9c22a9a48dfb27543b63dcd10d99158704
naru-T <narumida@gmail.com>
2018-07-09 16:28:37 +0900

    write_gpkg added

--- Misc/useful_functions.r
+++ Misc/useful_functions.r
@@ -33,4 +33,22 @@ sp.na.omit <- function(x, margin=1) {
 #Example
 #data.sp <- sp.na.omit(data.sp)
 
+##Write data as a geopackage format --------------------------------------------------
+##based on tmaptools::write_shape
+
+write_gpkg <- function (shp, file) {
+  shpname <- deparse(substitute(shp))
+  dir <- dirname(file)
+  base <- basename(file)
+  if (!file.exists(dir)) 
+    stop("unknown directory", call. = FALSE)
+  if (substr(base, nchar(base) - 4, nchar(base)) == ".gpkg") 
+    base <- substr(base, 1, nchar(base) - 5)
+  if (inherits(shp, c("sf", "sfc"))) 
+    shp <- as(shp, "Spatial")
+  if (!inherits(shp, "Spatial")) 
+    stop("shpname is not a Spatial object", call. = FALSE)
+  rgdal::writeOGR(obj=shp, dsn = dir %+% "/" %+% file, layer = base, driver = "GPKG", overwrite_layer = TRUE)
+}
+
 
